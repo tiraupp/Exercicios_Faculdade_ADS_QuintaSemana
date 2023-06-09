50403d9a3a68d094068e0dbb024695ab66b720f9
Tiago Raupp <tiraupp@gmail.com>
2023-06-09 10:11:47 -0300

    correção main

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Exercicios_Faculdade_ADS_QuintaSemana
+# Exercicios Faculdade ADS ULBRA - Quinta semana de estudos.
+
+Atividade Prática Avaliativa
+
+1 - Crie uma classe que simule a jogada de um dado de seis lados (números de 1 a 6) por três vezes. Ao final some seus valores e apresente o resultado das três jogadas.
+
+2 - Uma farmácia precisa ajustar os preços de seus produtos em 12%. Elabore uma classe que receba o valor do produto e aplique o percentual de acréscimo. O novo valor a ser calculado deve ser arredondado para mais ou para menos usando o método round. A classe deve também conter um laço de repetição que encerre o programa quando o usuário fornecer o valor zero (0) para o valor do produto. Dessa forma, o usuário digita o valor do produto, a classe calcula e mostra o valor com acréscimo, a seguir solicita um novo valor. Esse processo continua enquanto o valor do produto for diferente de zero; caso contrário o programa será encerrado
+
+3 - Crie uma classe que gera um número aleatoriamente (entre 5 e 10) por Math.random. Em seguida, faça com que apareça em tela uma senha numérica contendo a mesma quantidade de dígitos correspondentes ao valor aleatório gerado. Apresente em tela o número sorteado e a senha.
+
+4 - Construa uma classe que receba uma frase qualquer e mostre essa frase de trás para a frente e sem espaços em branco,
+
+5 - Elabore uma classe que receba uma frase e verique que se essa frase possui palavras impróprias. As palavras impróprias são: sexo e sexual. Caso encontre uma dessas palavras, emita em tela a mensagem “conteúdo impróprio”, caso contrário “conteúdo liberado”,
+
+6 - Crie uma classe que exiba o substring de uma string FRASE composto por todos os caracteres de FRASE exceto o último caracter.
+
+7 - Para elevar um número a uma potência, usamos o método _______ de Math.
+
+8 - Para converter radianos em graus, usamos o método _______ de Math.
--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,15 @@ public class Main {
         ElevarPotencia elevarPotencia = new ElevarPotencia();
         RadianosEmGrau radianosEmGrau = new RadianosEmGrau();
 
-        String [] opcoes = {"1 - Dados", "2 - Farmacia", "3 - Senha", "4 - Frase Invertida", "5 - Verifica Conteúdo da Frase", "6 - Frase sem último caracter", "7 - Número elevado a potência", "8 - Conversão de radianos em grau", "Sair"};
+        String[] opcoes = {"1 - Dados", "2 - Farmacia", "3 - Senha", "4 - Frase Invertida", "5 - Verifica Conteúdo da Frase", "6 - Frase sem último caracter", "7 - Número elevado a potência", "8 - Conversão de radianos em grau", "Sair"};
 
         do {
-            escolha = (String) JOptionPane.showInputDialog( null , "Escolha o exercicio: " , "Exercicios" , JOptionPane.QUESTION_MESSAGE , null , opcoes, "1 - Dados");
+            escolha = (String) JOptionPane.showInputDialog(null, "Escolha o exercicio: ", "Exercicios", JOptionPane.QUESTION_MESSAGE, null, opcoes, "1 - Dados");
 
             switch (escolha) {
                 case "Sair":
                     escolha = "";
-                    JOptionPane.showMessageDialog(null,"Saindo da aplicação", "Sair", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Saindo da aplicação", "Sair", JOptionPane.INFORMATION_MESSAGE);
                     break;
                 case "1 - Dados":
                     dado.mostrarJogada();
@@ -52,155 +52,10 @@ public class Main {
                     radianosEmGrau.conversaoRadianosEmGrau();
                     break;
                 default:
-                    JOptionPane.showMessageDialog(null,"Exercício não existe: ");
+                    JOptionPane.showMessageDialog(null, "Exercício não existe: ");
             }
         } while (escolha != "");
     }
 
-
-    public static void desconto(){
-        Scanner sc = new Scanner(System.in);
-        float valorProduto, percentualDesconto, desconto;
-
-        System.out.println("Informe o valor do produto: ");
-        valorProduto = sc.nextFloat();
-        System.out.println("Informe o percentual de desconto: ");
-        percentualDesconto = sc.nextFloat();
-        desconto = (valorProduto * percentualDesconto) / 100;
-        System.out.println(" ");
-        System.out.println("O valor de desconto é: R$" + desconto);
-        System.out.println("O valor do produto com desconto: R$" + (valorProduto - desconto));
-    }
-
-
-
-    public static void itbi(){
-        float valorVenal, valorTransacao, maiorValor, percentualImposto, imposto;
-        valorVenal = parseFloat(JOptionPane.showInputDialog("Informe o valor venal do bem "));
-        valorTransacao = parseFloat(JOptionPane.showInputDialog("Informe o valor negociado "));
-        percentualImposto = parseFloat(JOptionPane.showInputDialog("Informe o percentual de imposto "));
-        if(valorVenal > valorTransacao) {
-            maiorValor = valorVenal;
-        } else {
-            maiorValor = valorTransacao;
-        }
-        imposto = maiorValor * percentualImposto / 100;
-        JOptionPane.showMessageDialog(null,"O valor do ITBI a ser recolhido é de : R$" + imposto);
-    }
-
-    public static void mediaNotas(){
-        float notaProva1, notaProva2, notaTrabalho, media;
-
-        notaProva1 = parseFloat(JOptionPane.showInputDialog("Informe a nota da primeira prova"));
-        notaProva2 = parseFloat(JOptionPane.showInputDialog("Informe a nota da segunda prova"));
-        notaTrabalho = parseFloat(JOptionPane.showInputDialog("Informe a nota do trabalho"));
-        media = (notaProva1+notaProva2+notaTrabalho)/3;
-        if(media > 6) {
-            JOptionPane.showMessageDialog(null,"O aluno foi Aprovado com média " + media);
-        } else {
-            JOptionPane.showMessageDialog(null,"O aluno foi Reprovado com média " + media);
-        }
-
-    }
-
-    public static void aposentadoria() {
-        int idade, tempoContribuicao, sexo, tempoRestante, idadeRestante;
-
-        idade = parseInt(JOptionPane.showInputDialog("Informe a idade"));
-        tempoContribuicao = parseInt(JOptionPane.showInputDialog("Informe o tempo de contribuição previdenciaria"));
-        do {
-            sexo = parseInt(JOptionPane.showInputDialog("Informe o sexo: 1 - Masculino / 2 - Feminino"));
-            switch (sexo) {
-                case 1:
-                    if (tempoContribuicao < 35) {
-                        if (idade < 65) {
-                            tempoRestante = 35 - tempoContribuicao;
-                            idadeRestante = 65 - idade;
-                            if (tempoRestante < idadeRestante) {
-                                JOptionPane.showMessageDialog(null, "Restam " + tempoRestante + " anos de contribuição para se aposentar, você estará com " + (tempoRestante + idade) + " anos de idade!");
-                            } else {
-                                JOptionPane.showMessageDialog(null, "Restam " + idadeRestante + " anos para se aposentar por idade, com 65 anos!");
-                            }
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Você já tem o direito a aposentadoria por idade.");
-                        }
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Você já tem o direito a aposentadoria por tempo de contribuição.");
-                    }
-                    sexo = 0;
-                    break;
-                case 2:
-                    if (tempoContribuicao < 30) {
-                        if (idade < 60) {
-                            tempoRestante = 30 - tempoContribuicao;
-                            idadeRestante = 60 - idade;
-                            if (tempoRestante < idadeRestante) {
-                                JOptionPane.showMessageDialog(null, "Restam " + tempoRestante + " anos de contribuição para se aposentar, você estará com " + (tempoRestante + idade) + " anos de idade!");
-                            } else {
-                                JOptionPane.showMessageDialog(null, "Restam " + idadeRestante + " anos para se aposentar por idade, com 65 anos!");
-                            }
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Você já tem o direito a aposentadoria por idade.");
-                        }
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Você já tem o direito a aposentadoria por tempo de contribuição.");
-                    }
-                    sexo = 0;
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Sexo informado inválido, tente novamente!");
-                    break;
-            }
-
-        } while (sexo != 0);
-    }
-
-    public static void resistencia(){
-        float [] resistecias = new float[4];
-        float maiorResistecia = 0, menorResistencia = 1000000, total = 0;
-        int i;
-
-        for(i=0;i<resistecias.length;i++){
-            resistecias[i] = parseFloat(JOptionPane.showInputDialog("Informe o valor da resistência "+(i+1)));
-        }
-        for (i=0;i<resistecias.length;i++) {
-            total = total + resistecias[i];
-            if (resistecias[i] > maiorResistecia) {
-                maiorResistecia = resistecias[i];
-            }
-            if (resistecias[i] < menorResistencia) {
-                menorResistencia = resistecias[i];
-            }
-        }
-        JOptionPane.showMessageDialog(null,"A soma da resistência em séria é: " + total + "\na menor resistência: " + menorResistencia + "\na maior resistência: " + maiorResistecia );
-    }
-
-    public static void login(){
-        String loginOriginal = "java8", senhaOriginal = "java8";
-        String loginDigitado, senhaDigitada;
-        int tentativas = 3;
-        do {
-            loginDigitado = JOptionPane.showInputDialog("Informe o login");
-            senhaDigitada = JOptionPane.showInputDialog("digite sua senha");
-            if (loginDigitado.equals(loginOriginal) && senhaDigitada.equals(senhaOriginal)) {
-                JOptionPane.showMessageDialog(null,"Login efetuado com sucesso!");
-                break;
-            } else {
-                tentativas --;
-                JOptionPane.showMessageDialog(null,"Login ou senha digitados estão incorretos! Você possui mais " + tentativas + " tentativas. ");
-            }
-        } while (tentativas > 0);
-    }
-
-    public static void tabuada() {
-        int valor, i;
-        String tabuadaPronta = "";
-        valor = parseInt(JOptionPane.showInputDialog("Informe qual número deseja a tabuada: "));
-        tabuadaPronta = ("Tabuada do número " + valor + "\n");
-        for (i = 1; i <= 10; i++) {
-            tabuadaPronta = tabuadaPronta + (valor + " X " + i + " = " + (valor * i) + "\n");
-        }
-        JOptionPane.showMessageDialog(null, tabuadaPronta);
-    }
 
 }
